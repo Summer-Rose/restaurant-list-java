d678bdbbc402aa6efbca4364c2655db586ea1976
Summer Brochtrup & Momo Ozawa <student@epicodus.com>
2015-08-18 10:37:46 -0700

    display restaurants i've been to

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,7 +8,7 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-    	staticFileLocation("/public");
+    	// staticFileLocation("/public");
     	String layout = "templates/layout.vtl";
 
       get("/", (request, response) -> {
@@ -17,6 +17,31 @@ public class App {
 
         model.put("template", "templates/index.vtl");
         return new ModelAndView (model, layout);
+      }, new VelocityTemplateEngine());
+
+      post("/restaurant-list", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        // String sessionRestaurant = request.session().attribute("sessionRestaurants");
+        // String sessionQuadrant = request.session().attribute("sessionQuadrants")
+        // HashMap<String, String> rHashMap = request.session().attribute("rHashMap");
+
+        ArrayList<Restaurant> restaurantList = request.session().attribute("restaurantList");
+
+        if (restaurantList == null) {
+          restaurantList = new ArrayList<Restaurant>();
+          request.session().attribute("restaurantList", restaurantList);
+        }
+
+        String rName = request.queryParams("restaurantName");
+        String rQuadrant = (request.queryParams("quadrant")).toUpperCase();
+        Restaurant newRestaurant = new Restaurant(rName, rQuadrant);
+
+        restaurantList.add(newRestaurant);
+
+        model.put("restaurantList", request.session().attribute("restaurantList"));
+
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, layout);
       }, new VelocityTemplateEngine());
     }
 }
